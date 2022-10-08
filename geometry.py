6d8668aee7ccc597c9994d4e5ba8112ec2f18cd5
zezs <my.version.ctrl@gmail.com>
2022-10-08 22:34:30 +0200

    Member function created in in Geometry

--- geometry.py
+++ geometry.py
@@ -1,5 +1,17 @@
 class PPoint:
 
-    def __init__(self, x, y):
-        self.x=x
-        self.y=y
+    def __init__(self, x=1, y=1):
+        self.x = x
+        self.y = y
+        print("Constructor called!")
+
+    def setY(self, Y):
+        self.y=Y
+
+
+pp1 = PPoint()
+print(pp1.x)
+
+pp1.setY(10)
+
+print(pp1.y)
